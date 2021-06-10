4580fc93e6029d2eb003a3856c8c94ec6f29db66
Erika <erika.rasnick@gmail.com>
2021-06-10 15:11:30 -0400

    update to addNarrData v1.0.1; narr data downloads in chunks

--- narr.R
+++ narr.R
@@ -1,6 +1,6 @@
 #!/usr/local/bin/Rscript
 
-dht::greeting(geomarker_name = 'narr', version = '0.1', description = 'add NARR weather variables to geocoded data')
+dht::greeting(geomarker_name = 'narr', version = '0.2', description = 'add NARR weather variables to geocoded data')
 
 dht::qlibrary(dplyr)
 dht::qlibrary(tidyr)
@@ -25,20 +25,13 @@ dht::check_for_column(d, 'end_date', d$end_date)
 d$start_date <- dht::check_dates(d$start_date)
 d$end_date <- dht::check_dates(d$end_date)
 
-message('finding NARR grid cell for each point...')
-d_narr_cell <- addNarrData::get_narr_cell_numbers(d)
-
-if (!fs::file_exists('s3_downloads/geomarker/narr/narr.fst')) {
-   message('downloading narr.fst (> 22GB) ...')
-   s3::s3_get('s3://geomarker/narr/narr.fst')
-}
-
 message('appending NARR variables based on grid cell and date range...')
-d_out <- addNarrData::get_narr_data(d_narr_cell,
-                                    narr_fst_filepath = fs::path_wd("s3_downloads/geomarker/narr/narr.fst"))
+d_out <- addNarrData::get_narr_data(d, confirm = F)
 
 ## merge back on .row after unnesting .rows into .row
 dht::write_geomarker_file(d = d_out,
                      filename = opt$filename,
                      geomarker_name = 'narr',
-                     version = '0.1')
+                     version = '0.2')
+
+
