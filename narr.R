27e19d39caefd3f967a97cee125077d0b2d647dc
Erika <erika.rasnick@gmail.com>
2021-06-21 15:30:59 -0400

    temp and humidity by default, --all flag to get all narr variables

--- narr.R
+++ narr.R
@@ -11,12 +11,21 @@ dht::qlibrary(addNarrData)
 doc <- '
       Usage:
       narr.R <filename>
+      narr.R <filename> --all
+      narr.R (-h | --help)
+
+      Options:
+      -h --help   Show this screen
+      --all   Returns all narr variables
       '
 
 opt <- docopt::docopt(doc)
 ## for interactive testing
 ## opt <- docopt::docopt(doc, args = 'test/my_address_file_geocoded.csv')
 ## opt <- docopt::docopt(doc, args = 'my_address_file_geocoded.csv')
+####### to flag for all narr variables
+## opt <- docopt::docopt(doc, args = 'test/my_address_file_geocoded.csv --all')
+## opt <- docopt::docopt(doc, args = 'my_address_file_geocoded.csv --all')
 
 d <- dht::read_lat_lon_csv(opt$filename)
 
@@ -29,13 +38,19 @@ d$start_date <- dht::check_dates(d$start_date)
 d$end_date <- dht::check_dates(d$end_date)
 
 message('appending NARR variables based on grid cell and date range...')
-d_out1 <- addNarrData::get_narr_data(d, narr_variables = c("hpbl", "vis"), confirm = F)
-d_out2 <- addNarrData::get_narr_data(d, narr_variables = c("uwnd.10m", "vwnd.10m"), confirm = F)
-d_out3 <- addNarrData::get_narr_data(d, narr_variables = c("air.2m", "rhum.2m"), confirm = F)
-d_out4 <- addNarrData::get_narr_data(d, narr_variables = c("prate", "pres.sfc"), confirm = F)
+if (opt$all == FALSE) {
+  d_out <- addNarrData::get_narr_data(d, narr_variables = c("air.2m", "rhum.2m"), confirm = F)
+} else {
+  cli::cli_alert_warning("Due to their size, the narr data files will be downloaded in 4 groups of 2, which
+                         will be reflected in the progress messages below.")
+  d_out1 <- addNarrData::get_narr_data(d, narr_variables = c("hpbl", "vis"), confirm = F)
+  d_out2 <- addNarrData::get_narr_data(d, narr_variables = c("uwnd.10m", "vwnd.10m"), confirm = F)
+  d_out3 <- addNarrData::get_narr_data(d, narr_variables = c("air.2m", "rhum.2m"), confirm = F)
+  d_out4 <- addNarrData::get_narr_data(d, narr_variables = c("prate", "pres.sfc"), confirm = F)
 
-d_out <- bind_rows(d_out1, d_out2, d_out3, d_out4) %>%
-  select(-.row)
+  d_out <- bind_rows(d_out1, d_out2, d_out3, d_out4) %>%
+    select(-.row)
+}
 
 ## merge back on .row after unnesting .rows into .row
 dht::write_geomarker_file(d = d_out,
