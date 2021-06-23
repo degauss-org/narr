ca3a0d453b810a4a488505f1ace5136a3b5e15ea
Erika <erika.rasnick@gmail.com>
2021-06-23 11:50:49 -0400

    clean up messages and logical code

--- narr.R
+++ narr.R
@@ -38,11 +38,11 @@ d$start_date <- dht::check_dates(d$start_date)
 d$end_date <- dht::check_dates(d$end_date)
 
 message('appending NARR variables based on grid cell and date range...')
-if (opt$all == FALSE) {
+if (!opt$all) {
   d_out <- addNarrData::get_narr_data(d, narr_variables = c("air.2m", "rhum.2m"), confirm = F)
 } else {
-  cli::cli_alert_warning("Due to their size, the narr data files will be downloaded in 4 groups of 2, which
-                         will be reflected in the progress messages below.")
+  cli::cli_alert_warning("Due to their size, the narr data files will be downloaded and processed in 4 groups of 2,
+                         which will be reflected in the progress messages below.")
   d_out1 <- addNarrData::get_narr_data(d, narr_variables = c("hpbl", "vis"), confirm = F)
   d_out2 <- addNarrData::get_narr_data(d, narr_variables = c("uwnd.10m", "vwnd.10m"), confirm = F)
   d_out3 <- addNarrData::get_narr_data(d, narr_variables = c("air.2m", "rhum.2m"), confirm = F)
