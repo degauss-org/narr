3bb5225fa290d27e20b06c1cadb1496236ea067c
Erika <erika.rasnick@gmail.com>
2022-03-28 15:34:56 -0400

    degaussverse 1.0 updates

--- entrypoint.R
+++ entrypoint.R
@@ -1,12 +1,13 @@
 #!/usr/local/bin/Rscript
 
-dht::greeting(geomarker_name = 'narr', version = '0.2', description = 'add NARR weather variables to geocoded data')
+dht::greeting()
 
-dht::qlibrary(dplyr)
-dht::qlibrary(tidyr)
-dht::qlibrary(sf)
-dht::qlibrary(data.table)
-dht::qlibrary(addNarrData)
+## load libraries without messages or warnings
+withr::with_message_sink("/dev/null", library(dplyr))
+withr::with_message_sink("/dev/null", library(tidyr))
+withr::with_message_sink("/dev/null", library(sf))
+withr::with_message_sink("/dev/null", library(data.table))
+withr::with_message_sink("/dev/null", library(addNarrData))
 
 doc <- '
       Usage:
@@ -20,26 +21,26 @@ doc <- '
       '
 
 opt <- docopt::docopt(doc)
+
 ## for interactive testing
 ## opt <- docopt::docopt(doc, args = 'test/my_address_file_geocoded.csv')
-## opt <- docopt::docopt(doc, args = 'my_address_file_geocoded.csv')
-####### to flag for all narr variables
-## opt <- docopt::docopt(doc, args = 'test/my_address_file_geocoded.csv --all')
-## opt <- docopt::docopt(doc, args = 'my_address_file_geocoded.csv --all')
 
+message("reading input file...")
 d <- dht::read_lat_lon_csv(opt$filename)
 
-dht::check_for_column(d, 'lat', d$lat)
-dht::check_for_column(d, 'lon', d$lon)
+dht::check_for_column(d, "lat", d$lat)
+dht::check_for_column(d, "lon", d$lon)
 dht::check_for_column(d, 'start_date', d$start_date)
 dht::check_for_column(d, 'end_date', d$end_date)
 
 d$start_date <- dht::check_dates(d$start_date)
 d$end_date <- dht::check_dates(d$end_date)
 
+## add code here to calculate geomarkers
 message('appending NARR variables based on grid cell and date range...')
 if (!opt$all) {
   d_out <- addNarrData::get_narr_data(d, narr_variables = c("air.2m", "rhum.2m"), confirm = F)
+  arg_file_name <- ""
 } else {
   cli::cli_alert_warning("Due to their size, the narr data files will be downloaded and processed in 4 groups of 2,
                          which will be reflected in the progress messages below.")
@@ -48,14 +49,19 @@ if (!opt$all) {
   d_out3 <- addNarrData::get_narr_data(d, narr_variables = c("air.2m", "rhum.2m"), confirm = F)
   d_out4 <- addNarrData::get_narr_data(d, narr_variables = c("prate", "pres.sfc"), confirm = F)
 
-  d_out <- bind_rows(d_out1, d_out2, d_out3, d_out4) %>%
-    select(-.row)
+  suppressMessages(
+    d_out <- left_join(d_out1, d_out2) %>%
+      left_join(d_out3) %>%
+      left_join(d_out4) %>%
+      select(-.row)
+  )
+
+  arg_file_name <- "all_vars"
 }
 
 ## merge back on .row after unnesting .rows into .row
 dht::write_geomarker_file(d = d_out,
-                     filename = opt$filename,
-                     geomarker_name = 'narr',
-                     version = '0.2')
+                          filename = opt$filename,
+                          argument = arg_file_name)
 
 
