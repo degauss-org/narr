041dd66e4ae3da0f5219ff9230ad3ad27af61e5f
erikarasnick <erika.rasnick@gmail.com>
2025-05-15 10:57:25 -0400

    use appc instead of addNarrData

--- README.md
+++ README.md
@@ -8,10 +8,10 @@
 If `my_address_file_geocoded.csv` is a file in the current working directory with coordinate columns named `lat`, `lon`, `start_date`, and `end_date` then the [DeGAUSS command](https://degauss.org/using_degauss.html#DeGAUSS_Commands):
 
 ```sh
-docker run --rm -v $PWD:/tmp ghcr.io/degauss-org/narr:0.4.0 my_address_file_geocoded.csv
+docker run --rm -v $PWD:/tmp ghcr.io/degauss-org/narr:1.0.0 my_address_file_geocoded.csv
 ```
 
-will produce `my_address_file_geocoded_narr_0.4.0_weather.csv` with added columns:
+will produce `my_address_file_geocoded_narr_1.0.0_weather.csv` with added columns:
 
 - **`air.2m`**: air temperature at 2m
 - **`rhum.2m`**: humidity at 2m
@@ -25,36 +25,23 @@ Users can supply an optional argument to select which NARR variables are returne
 | `weather` (default) | `air.2m` <br> `rhum.2m`      | air temperature at 2m <br> humidity at 2m       |
 | `wind`      | `uwnd.10m` <br> `vwnd.10m`  | U wind speed at 10m <br>  V wind speed at 10m     |
 | `atmosphere`      | `hpbl` <br> `vis`  | planetary boundary layer height <br> visibility    |
-| `pratepres`      | `prate` <br> `pres.sfc`  | precipitation rate <br> surface pressure    |
-| `none`      | `narr_cell`  |  NARR raster cell number |
+| `precippres`      | `acpcp` <br> `pres.sfc`  | accumulated total precipitation <br> surface pressure    |
 
 For example, 
 
 ```sh
-docker run --rm -v $PWD:/tmp ghcr.io/degauss-org/narr:0.4.0 my_address_file_geocoded.csv wind 
+docker run --rm -v $PWD:/tmp ghcr.io/degauss-org/narr:1.0.0 my_address_file_geocoded.csv wind 
 ```
 
 will return `uwnd.10m` and `vwnd.10m`. 
-
-This argument can also be used to only return the `narr_cell`, which is the NARR raster cell number, to be used for later NARR data lookups:
-
-```sh
-docker run --rm -v $PWD:/tmp ghcr.io/degauss-org/narr:0.4.0 my_address_file_geocoded.csv none
-```
 
 ### Docker RAM requirements
 
 If this container errors unexpectedly, you may need to allocate more RAM to Docker. Please see the [troubleshooting guide](https://degauss.org/troubleshooting.html#Insufficient_Memory) for more information.
 
-## Geomarker Methods
+## Geomarker Methods and Data
 
-This container was built using the [addNarrData](https://geomarker.io/addNarrData/) package.
-
-## Geomarker Data
-
-- Daily weather data was downloaded from [NARR](https://www.ncei.noaa.gov/products/weather-climate-models/north-american-regional).
-- NARR data is stored as [fst](https://github.com/fstpackage/fst) files at s3://geomarker/narr/narr_chunk_fst/.
-- Detailed information for how data was converted to chunk files can be found [here](https://github.com/geomarker-io/narr_raster_to_fst).
+This container was built using the [appc](https://geomarker.io/appc) package.
 
 ## DeGAUSS Details
 
--- entrypoint.R
+++ entrypoint.R
@@ -6,8 +6,7 @@ dht::greeting()
 withr::with_message_sink("/dev/null", library(dplyr))
 withr::with_message_sink("/dev/null", library(tidyr))
 withr::with_message_sink("/dev/null", library(sf))
-withr::with_message_sink("/dev/null", library(data.table))
-withr::with_message_sink("/dev/null", library(addNarrData))
+withr::with_message_sink("/dev/null", library(dht))
 
 doc <- '
       Usage:
@@ -17,53 +16,74 @@ doc <- '
       Options:
       -h --help   Show this screen
       filename  name of csv file
-      vars   weather, wind, atmosphere, pratepres, or none (see readme for more info)
+      vars   weather, wind, atmosphere, or precippres (see readme for more info)
       '
 
 opt <- docopt::docopt(doc)
 
 ## for interactive testing
 ## opt <- docopt::docopt(doc, args = 'test/my_address_file_geocoded.csv')
-
-message("reading input file...")
 d <- dht::read_lat_lon_csv(opt$filename)
+
+# checks
 dht::check_for_column(d, "lat", d$lat)
 dht::check_for_column(d, "lon", d$lon)
+dht::check_for_column(d, 'start_date', d$start_date)
+dht::check_for_column(d, 'end_date', d$end_date)
+d$start_date <- dht::check_dates(d$start_date)
+d$end_date <- dht::check_dates(d$end_date)
+dht::check_end_after_start_date(d$start_date, d$end_date)
 
-if (is.null(opt$vars)) {
+if (is.null(opt$vars)) opt$vars <- "null"
+if (!opt$vars %in% c("weather", "wind", "atmosphere", "precippres")) {
   opt$vars <- "weather"
-  cli::cli_alert_warning("Blank argument for NARR variable selection. Will return air.2m and rhum.2m. Please see {.url https://degauss.org/narr/} for more information about the NARR variable argument.")
+  cli::cli_alert_warning(
+    "Blank or invalid argument for NARR variable selection. Will return air.2m and rhum.2m. Please see {.url https://degauss.org/narr/} for more information about the NARR variable argument."
+  )
 }
 
-if (! opt$vars %in% c("weather", "wind", "atmosphere", "pratepres", "none")) {
-  opt$vars <- "weather"
-  cli::cli_alert_warning("Invalid argument for NARR variable selection. Will return air.2m and rhum.2m. Please see {.url https://degauss.org/narr/} for more information about the NARR variable argument.")
-}
+narr_variables = case_when(
+  opt$vars == "weather" ~ c("air.2m", "rhum.2m"),
+  opt$vars == "wind" ~ c("uwnd.10m", "vwnd.10m"),
+  opt$vars == "atmosphere" ~ c("hpbl", "vis"),
+  opt$vars == "precippres" ~ c("acpcp", "pres.sfc")
+)
 
-if (opt$vars == "none") {
-  d_out <- addNarrData::get_narr_cell_numbers(d)
-} else {
-  dht::check_for_column(d, 'start_date', d$start_date)
-  dht::check_for_column(d, 'end_date', d$end_date)
-
-  d$start_date <- dht::check_dates(d$start_date)
-  d$end_date <- dht::check_dates(d$end_date)
-
-  narr_variables = case_when(
-    opt$vars == "weather" ~ c("air.2m", "rhum.2m"),
-    opt$vars == "wind" ~ c("uwnd.10m", "vwnd.10m"),
-    opt$vars == "atmosphere" ~ c("hpbl", "vis"),
-    opt$vars == "pratepres" ~ c("prate", "pres.sfc")
+message('linking to s2 cells...')
+options(timeout = 3600)
+d_s2 <-
+  d |>
+  mutate(s2 = s2::s2_lnglat(d$lon, d$lat) |> s2::as_s2_cell()) |>
+  filter(!is.na(s2)) |>
+  mutate(
+    dates = purrr::map2(
+      start_date,
+      end_date,
+      \(x, y) seq.Date(from = x, to = y, by = "day")
+    )
   )
 
-  message('appending NARR variables based on grid cell and date range...')
-  d_out <- addNarrData::get_narr_data(d, narr_variables = narr_variables, confirm = F)
-  d_out <- dplyr::select(d_out, -.row)
-}
+d_narr <-
+  purrr::map(
+    narr_variables,
+    \(narr_var)
+      d_s2 |>
+        mutate(
+          {{ narr_var }} := appc::get_narr_data(
+            x = s2,
+            dates = dates,
+            narr_var = narr_var
+          )
+        )
+  ) |>
+  purrr::reduce(.f = left_join) |>
+  tidyr::unnest(c(dates, all_of(narr_variables))) |>
+  select(.row = .row, dates, all_of(narr_variables))
 
-## merge back on .row after unnesting .rows into .row
-dht::write_geomarker_file(d = d_out,
-                          filename = opt$filename,
-                          argument = opt$vars)
+d_out <- left_join(d, d_narr, by = ".row")
 
-
+dht::write_geomarker_file(
+  d = d_out,
+  filename = opt$filename,
+  argument = opt$vars
+)
